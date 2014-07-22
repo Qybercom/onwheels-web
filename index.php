7378e8e1fe1f389b32b7148660ce3d956b419dbe
alex025 <saniafd93@gmail.com>
2014-07-22 20:38:29 +0300

    reset index

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $config->Culture(new QuarkCultureRU());
 
 $mongoSource = new QuarkCredentials('mongodb');
 $mongoSource->Endpoint('127.0.0.1', 27017);
-$mongoSource->User('webapp', 'onwheels!web');
+$mongoSource->User('user', 'pass');
 $mongoSource->Resource('onwheels');
 
 $mongoDB = new \Quark\Extensions\Mongo\Config();
@@ -19,8 +19,8 @@ $mongoDB->Source('main', $mongoSource);
 
 $config->Extension($mongoDB);
 $config->Extension(new \Quark\Extensions\Facebook\Config(
-	'678583925524753',
-	'0f6e778d59e8a74b5efb78b434cb9c46'
+	'app_id',
+	'app_secret'
 ));
 
 Quark::On(Quark::EVENT_HTTP_EXCEPTION, function () {
