9ebec45bf53c9c56dc6e14e03a267a99a538d194
Alex Furnica <saniafd93@gmail.com>
2014-09-05 00:18:02 +0300

    patched

--- index.php
+++ index.php
@@ -8,17 +8,17 @@ use Quark\QuarkCredentials;
 $config = new QuarkConfig();
 
 $mongoSource = new QuarkCredentials('mongodb');
-$mongoSource->Endpoint('127.0.0.1', 27017);
-$mongoSource->User('webapp', 'onwheels!web');
-$mongoSource->Resource('onwheels');
+$mongoSource->Endpoint('host', 27017);
+$mongoSource->User('login', 'password');
+$mongoSource->Resource('database');
 
 $mongoDB = new \Quark\Extensions\Mongo\Config();
 $mongoDB->Source('main', $mongoSource);
 
 $config->Extension($mongoDB);
 $config->Extension(new \Quark\Extensions\Facebook\Config(
-	'678583925524753',
-	'0f6e778d59e8a74b5efb78b434cb9c46'
+	'id',
+	'secret'
 ));
 
 Quark::Run($config);
